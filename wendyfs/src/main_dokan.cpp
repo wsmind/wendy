cc5c4eec9538a2d1ffa20293cdf53f157f5e6296
Remi Papillie <remi.papillie@gmail.com>
2011-07-19 22:31:08 +0200

    windows implementation of directories

--- wendyfs/src/main_dokan.cpp
+++ wendyfs/src/main_dokan.cpp
@@ -31,11 +31,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dokan.h>
+#include <iostream>
+
 #include <wendy/Project.hpp>
 
 #include "ProjectProxy.hpp"
 
 static ProjectProxy *proxy = NULL;
+
+static std::string wideToUtf8(std::wstring wide)
+{
+	// passing no output buffer return required buffer size
+	int size = WideCharToMultiByte(CP_UTF8, 0, wide.c_str(), -1, NULL, 0, NULL, NULL);
+	
+	// convert into a temporary buffer
+	char *outputBuffer = new char[size];
+	WideCharToMultiByte(CP_UTF8, 0, wide.c_str(), -1, outputBuffer, size, NULL, NULL);
+	
+	// create the equivalent string object
+	std::string utf = outputBuffer;
+	
+	// destroy temporary buffer
+	delete outputBuffer;
+	
+	return utf;
+}
+
+static std::wstring utf8ToWide(std::string utf)
+{
+	// passing no output buffer return required buffer size
+	int size = MultiByteToWideChar(CP_UTF8, 0, utf.c_str(), -1, NULL, 0);
+	
+	// convert into a temporary buffer
+	wchar_t *outputBuffer = new wchar_t[size];
+	MultiByteToWideChar(CP_UTF8, 0, utf.c_str(), -1, outputBuffer, size);
+	
+	// create the equivalent wstring object
+	std::wstring wide = outputBuffer;
+	
+	// destroy temporary buffer
+	delete outputBuffer;
+	
+	return wide;
+}
+
+// convert to utf-8, change '\\' to '/' and remove leading slash
+static std::string makePathStandard(LPCWSTR path)
+{
+	std::string utfPath = wideToUtf8(std::wstring(path));
+	
+	// change backslashes to forward slashes
+	size_t slashPos;
+	while ((slashPos = utfPath.find('\\')) != std::string::npos)
+		utfPath.replace(slashPos, 1, "/");
+	
+	// remove leading slash
+	return utfPath.substr(1);
+}
 
 static int DOKAN_CALLBACK WendyCreateFile(LPCWSTR filename, DWORD accessMode, DWORD shareMode, DWORD creationDisposition, DWORD flagsAndAttributes, PDOKAN_FILE_INFO info)
 {
@@ -46,13 +98,25 @@ static int DOKAN_CALLBACK WendyCreateFile(LPCWSTR filename, DWORD accessMode, DW
 static int DOKAN_CALLBACK WendyOpenDirectory(LPCWSTR filename, PDOKAN_FILE_INFO info)
 {
 	wprintf(L"OpenDir %s\n", filename);
+	std::cout << "OpenDir: " << makePathStandard(filename) << std::endl;
 	
-	if (wcscmp(filename, L"\\") == 0)
+	std::string path = makePathStandard(filename);
+	
+	ProjectProxy::FileAttributes attributes;
+	if (!proxy->getFileAttributes(path, &attributes))
+		return -ERROR_PATH_NOT_FOUND;
+	
+	if (!attributes.folder)
+		return -ERROR_PATH_NOT_FOUND; // TODO: find the right error code for this
+	
+	return 0;
+	
+	/*if (wcscmp(filename, L"\\") == 0)
 		return 0;
 	else if (wcscmp(filename, L"\\plop") == 0)
 		return 0;
 	
-	return -ERROR_PATH_NOT_FOUND;
+	return -ERROR_PATH_NOT_FOUND;*/
 }
 
 static int DOKAN_CALLBACK WendyCleanup(LPCWSTR filename, PDOKAN_FILE_INFO info)
@@ -62,38 +126,31 @@ static int DOKAN_CALLBACK WendyCleanup(LPCWSTR filename, PDOKAN_FILE_INFO info)
 	return 0;
 }
 
-static int DOKAN_CALLBACK WendyFindFiles(LPCWSTR filename, PFillFindData	fillFindData, PDOKAN_FILE_INFO info)
+static int DOKAN_CALLBACK WendyFindFiles(LPCWSTR filename, PFillFindData fillFindData, PDOKAN_FILE_INFO info)
 {
-	WIN32_FIND_DATAW entry;
-	
 	wprintf(L"FindFiles %s\n", filename);
 	
-	if (wcscmp(filename, L"\\") != 0)
-		return -ERROR_PATH_NOT_FOUND;
-	
+	WIN32_FIND_DATAW entry;
 	ZeroMemory(&entry, sizeof(WIN32_FIND_DATAW));
 	
-	std::vector<std::string> files = proxy->listDirectory("");
+	std::string path = makePathStandard(filename);
+	
+	std::vector<std::string> files = proxy->listFolder(path);
 	for (unsigned int i = 0; i < files.size(); ++i)
 	{
+		// filename
 		const std::string &filename = files[i];
+		std::wstring wfilename = utf8ToWide(filename);
+		wcsncpy(entry.cFileName, wfilename.c_str(), MAX_PATH - 1);
 		
-		entry.dwFileAttributes = FILE_ATTRIBUTE_NORMAL;
-		MultiByteToWideChar(CP_UTF8, 0, filename.c_str(), -1, entry.cFileName, 200);
+		// other attributes
+		ProjectProxy::FileAttributes attributes;
+		proxy->getFileAttributes(files[i], &attributes);
+		entry.dwFileAttributes = attributes.folder ? FILE_ATTRIBUTE_DIRECTORY : FILE_ATTRIBUTE_NORMAL;
+		
+		// send item back to caller
 		fillFindData(&entry, info);
 	}
-	
-	/*entry.dwFileAttributes = FILE_ATTRIBUTE_DIRECTORY;
-	wcscpy(entry.cFileName, L"plop");
-	fillFindData(&entry, info);
-	
-	entry.dwFileAttributes = FILE_ATTRIBUTE_NORMAL;
-	wcscpy(entry.cFileName, L"test");
-	fillFindData(&entry, info);
-	
-	entry.dwFileAttributes = FILE_ATTRIBUTE_NORMAL;
-	wcscpy(entry.cFileName, L"kitten");
-	fillFindData(&entry, info);*/
 	
 	return 0;
 }
