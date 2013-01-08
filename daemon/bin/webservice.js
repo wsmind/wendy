7b9cee847fdabafb27b3118c58a0af85e1c085c0
Remi Papillie <remi.papillie@gmail.com>
2013-01-08 23:13:31 +0100

    refactored webservice api; added basic generated documentation for it (/_api)

--- daemon/bin/webservice.js
+++ daemon/bin/webservice.js
@@ -26,32 +26,6 @@
 var http = require("http")
 var querystring = require("querystring")
 
-// HTTP API declaration
-// This structure is used to validate requests before
-// attempting any further processing. It does not perform
-// advanced checks, such as parameter type validation or
-// regex matching.
-// All query parameters are considered optional, none
-// are enforced as mandatory.
-var api = {
-	"data": {
-		"GET": ["version"],
-		"PUT": []
-	},
-	"list": {
-		"GET": []
-	},
-	"share": {
-		"POST": []
-	},
-	"version": {
-		"GET":  []
-	},
-	"tools": {
-		"GET": []
-	}
-}
-
 function WebService(engine, port)
 {
 	this.engine = engine
@@ -64,22 +38,24 @@ function WebService(engine, port)
 		var url = require("url").parse(request.url, true)
 		
 		var pathParts = url.pathname.split("/")
+		pathParts.shift() // remove first (blank) part
+		
+		var category = pathParts.shift()
+		var resource = "/" + pathParts.join("/")
 		
 		// basic API check
-		if (!self._validate(pathParts[1], request.method, url.query))
+		if (!self._validate(category, request.method, url.query))
 		{
 			response.writeHead(418, "I'm a teapot")
 			response.end()
 			return
 		}
 		
-		// forge method name
-		var methodName = "_" + pathParts[1] + "_" + request.method
+		// find the appropriate handler
+		var handler = self._api[category][request.method].handler
 		
-		var resource = "/" + pathParts.slice(2).join("/")
-		
-		// call it
-		self[methodName](resource, url.query, request, response)
+		// call it with the right _this_
+		handler.call(self, resource, url.query, request, response)
 	})
 	
 	server.listen(port, function()
@@ -91,17 +67,17 @@ exports.WebService = WebService
 
 WebService.prototype._validate = function(category, method, parameters)
 {
-	var allowedMethods = api[category]
+	var allowedMethods = this._api[category]
 	if (allowedMethods === undefined)
 		return false // unknown call semantics
 	
-	var allowedParameters = allowedMethods[method]
-	if (allowedParameters === undefined)
+	var method = allowedMethods[method]
+	if (method === undefined)
 		return false // method not allowed
 	
 	for (var parameterName in parameters)
 	{
-		if (allowedParameters.indexOf(parameterName) == -1)
+		if (!(parameterName in method.parameters))
 			return false // parameter not allowed
 	}
 	
@@ -109,343 +85,210 @@ WebService.prototype._validate = function(category, method, parameters)
 	return true
 }
 
-WebService.prototype._data_GET = function(resource, parameters, request, response)
-{
-	// try to read asset
-	this.engine.read(resource, function(err, stream)
-	{
-		if (err)
-		{
-			response.writeHead(400, err.message)
-			response.end()
-		}
-		else
-		{
-			response.writeHead(200, "OK")
-			stream.pipe(response)
-		}
-	})
-}
-
-WebService.prototype._data_PUT = function(resource, parameters, request, response)
-{
-	// try to write asset
-	this.engine.save(resource, request, function(err)
-	{
-		if (err)
-		{
-			response.writeHead(400, err.message)
-			response.end()
-		}
-		else
-		{
-			response.writeHead(200, "OK")
-			response.end()
-		}
-	})
-}
-
-WebService.prototype._list_GET = function(resource, parameters, request, response)
-{
-	// enumerate assets with the given filter
-	this.engine.list(resource, function(err, pathList)
-	{
-		if (err)
-		{
-			response.writeHead(400, err.message)
-			response.end()
-		}
-		else
-		{
-			response.writeHead(200, "OK")
-			response.write(JSON.stringify(pathList))
-			response.end()
-		}
-	})
-}
-
-WebService.prototype._share_POST = function(resource, parameters, request, response)
-{
-	var self = this
-	var body = ""
-	
-	request.on("data", function(chunk)
-	{
-		body += chunk
-	})
-	
-	request.on("end", function()
-	{
-		var postData = querystring.decode(body)
-		
-		// check that the description field is present
-		if (postData["description"] === undefined)
-		{
-			response.writeHead(400, "Required parameter: 'description'")
-			response.end()
-		}
-		else
-		{
-			// try to share everything local
-			self.engine.share(postData["description"], function(err, version)
+// HTTP API definition
+// This structure is used to validate requests before
+// attempting any further processing and generate documentation,
+// as well as handling requests as appropriate.
+// No advanced checks are performed, such as parameter
+// type validation or regex matching.
+// All query parameters are considered optional, none
+// are enforced as mandatory.
+WebService.prototype._api = {
+	"data": {
+		"GET": {
+			doc: "Retrieve asset raw content",
+			parameters: {
+				"/path": "path to asset",
+				"version": "id of a specific version to fetch. The latest version available will be retrieved if not specified."
+			},
+			handler: function(resource, parameters, request, response)
 			{
-				if (err) throw err
+				// try to read asset
+				this.engine.read(resource, function(err, stream)
+				{
+					if (err)
+					{
+						response.writeHead(400, err.message)
+						response.end()
+					}
+					else
+					{
+						response.writeHead(200, "OK")
+						stream.pipe(response)
+					}
+				})
+			}
+		},
+		"PUT": {
+			doc: "Save new content for asset",
+			parameters: {
+				"/path": "path to asset"
+			},
+			handler: function(resource, parameters, request, response)
+			{
+				// try to write asset
+				this.engine.save(resource, request, function(err)
+				{
+					if (err)
+					{
+						response.writeHead(400, err.message)
+						response.end()
+					}
+					else
+					{
+						response.writeHead(200, "OK")
+						response.end()
+					}
+				})
+			}
+		}
+	},
+	"list": {
+		"GET": {
+			doc: "List asset paths",
+			parameters: {
+				"/pattern": "glob-style expression to select a subset of asset paths"
+			},
+			handler: function(resource, parameters, request, response)
+			{
+				// enumerate assets with the given filter
+				this.engine.list(resource, function(err, pathList)
+				{
+					if (err)
+					{
+						response.writeHead(400, err.message)
+						response.end()
+					}
+					else
+					{
+						response.writeHead(200, "OK")
+						response.write(JSON.stringify(pathList))
+						response.end()
+					}
+				})
+			}
+		}
+	},
+	"share": {
+		"POST": {
+			doc: "Publish local changes",
+			parameters: {},
+			handler: function(resource, parameters, request, response)
+			{
+				var self = this
+				var body = ""
 				
-				console.log("version " + version + " successfully shared")
+				request.on("data", function(chunk)
+				{
+					body += chunk
+				})
 				
-				response.writeHead(200, "OK", {"Content-Type": "application/json"})
-				response.write(JSON.stringify({version: version}))
+				request.on("end", function()
+				{
+					var postData = querystring.decode(body)
+					
+					// check that the description field is present
+					if (postData["description"] === undefined)
+					{
+						response.writeHead(400, "Required parameter: 'description'")
+						response.end()
+					}
+					else
+					{
+						// try to share everything local
+						self.engine.share(postData["description"], function(err, version)
+						{
+							if (err) throw err
+							
+							console.log("version " + version + " successfully shared")
+							
+							response.writeHead(200, "OK", {"Content-Type": "application/json"})
+							response.write(JSON.stringify({version: version}))
+							response.end()
+						})
+					}
+				})
+			}
+		}
+	},
+	"version": {
+		"GET": {
+			doc: "Fetch metadata about a specific version",
+			parameters: {
+				"/id": "the version id (js time value), or the string \"local\" for local (unshared) changes"
+			},
+			handler: function(resource, parameters, request, response)
+			{
+				// try to read version
+				this.engine.readVersion(resource.slice(1), function(err, version)
+				{
+					if (err)
+					{
+						response.writeHead(400, err.message)
+						response.end()
+					}
+					else
+					{
+						response.writeHead(200, "OK", {
+							"Content-Type": "application/json"
+						})
+						response.end(JSON.stringify(version))
+					}
+				})
+			}
+		}
+	},
+	"tools": {
+		"GET": {
+			doc: "Access web tools",
+			parameters: {
+				"/name": "name of the tool to run"
+			},
+			handler: function(resource, parameters, request, response)
+			{
+				// rewrite the special /tools URL
+				this._api.data.GET.handler.call(this, "/.tools" + resource, parameters, request, response)
+			}
+		}
+	},
+	"_api": {
+		"GET": {
+			doc: "This API generated documentation",
+			parameters: {},
+			handler: function(resource, parameters, request, response)
+			{
+				response.writeHead(200, "OK", {
+					"Content-Type": "text/html"
+				})
+				
+				// basic documentation generation
+				response.write("<!DOCTYPE html>\n")
+				response.write("<html>\n")
+				response.write("<body style=\"font-family: sans-serif\">\n")
+				response.write("<h1>Wendy webservice API</h1>\n")
+				
+				for (var categoryName in this._api)
+				{
+					var category = this._api[categoryName]
+					for (var methodName in category)
+					{
+						var method = category[methodName]
+						response.write("<h2>" + methodName + " /" + categoryName + "</h2>\n")
+						response.write("<p>" + method.doc + "</p>\n")
+						response.write("<dl>\n")
+						for (var parameterName in method.parameters)
+						{
+							response.write("<dt style=\"font-weight: bold\">" + parameterName + "</dt>\n")
+							response.write("<dd>" + method.parameters[parameterName] + "</dd>\n")
+						}
+						response.write("<dl>\n")
+					}
+				}
+				
+				response.write("</body>\n")
+				response.write("</html>\n")
 				response.end()
-			})
-		}
-	})
-}
-
-WebService.prototype._version_GET = function(resource, parameters, request, response)
-{
-	// try to read version
-	this.engine.readVersion(resource.slice(1), function(err, version)
-	{
-		if (err)
-		{
-			response.writeHead(400, err.message)
-			response.end()
-		}
-		else
-		{
-			response.writeHead(200, "OK", {
-				"Content-Type": "application/json"
-			})
-			response.end(JSON.stringify(version))
-		}
-	})
-}
-
-WebService.prototype._tools_GET = function(resource, parameters, request, response)
-{
-	// rewrite the special /tools URL
-	this._data_GET("/.tools" + resource, parameters, request, response)
-}
-
-/*Service.prototype.handleClient = function(client)
-{
-	var self = this
-	
-	console.log("client connected")
-	
-	var assetCallback = function(id, asset)
-	{
-		self.sendAsset(client, id, asset)
-	}
-	
-	// engine notifications
-	this.engine.on("changed", assetCallback)
-	
-	// disconnection
-	client.on("close", function()
-	{
-		console.log("client disconnected")
-		
-		// TODO: close all remaining AssetFiles
-		
-		self.engine.removeListener("changed", assetCallback)
-	})
-	
-	// client actions
-	var reader = new StreamReader(client)
-	var openedFiles = {} // fd -> cache.AssetFile
-	this.processAction(client, reader, openedFiles)
-	
-	// initial dump
-	this.engine.dump(assetCallback)
-}
-
-Service.prototype.processAction = function(client, reader, openedFiles)
-{
-	var self = this
-	reader.readLine(function(line)
-	{
-		var spacePosition = line.indexOf(" ")
-		var command = line.slice(0, spacePosition)
-		var parameters = line.slice(spacePosition + 1)
-		
-		switch (command)
-		{
-			case "CREATE":
-			{
-				var path = parameters
-				self.engine.create(path)
-				break
-			}
-			
-			case "LOCK":
-			{
-				var parts = parameters.split(" ")
-				var id = parts[0]
-				var application = parts[1]
-				console.log("LOCK!! -> " + id + ", " + application)
-				
-				self.engine.lock(id, application);
-				
-				break
-			}
-			
-			case "UNLOCK":
-			{
-				var id = parameters
-				console.log("UNLOCK!! -> " + id)
-				
-				self.engine.unlock(id);
-				
-				break
-			}
-			
-			case "OPEN":
-			{
-				var parts = parameters.split(" ")
-				var id = parts[0]
-				var mode = parts[1]
-				console.log("OPEN!! -> " + id + ", " + mode)
-				
-				var engineMode = null
-				if (mode == "READING") engineMode = "r"
-				else if (mode == "WRITING") engineMode = "w"
-				
-				self.engine.open(id, engineMode, function(err, file)
-				{
-					if (err) throw err
-					
-					// find a free file descriptor number
-					var fd = 0
-					while (openedFiles[fd] != undefined)
-						fd++;
-					
-					openedFiles[fd] = file
-					
-					self.safeWrite(client, "OPENED " + id + " " + mode + " " + fd + "\n")
-				})
-				
-				break;
-			}
-			
-			case "READ":
-			{
-				var parts = parameters.split(" ")
-				var fd = parseInt(parts[0])
-				var offset = parseInt(parts[1])
-				var size = parseInt(parts[2])
-				
-				console.log("READ!! -> " + fd + ", " + offset + ", " + size)
-				
-				if (openedFiles[fd] === undefined)
-				{
-					console.error("File " + fd + " was read but not opened!")
-					break;
-				}
-				
-				openedFiles[fd].read(new Buffer(size), offset, function(err, bytesRead, buffer)
-				{
-					if (err) throw err
-					
-					// TODO: handle bytesRead < size
-					
-					self.sendChunk(client, fd, offset, buffer)
-				})
-				
-				break;
-			}
-			
-			case "CLOSE":
-			{
-				var fd = parameters
-				
-				console.log("CLOSE!! -> " + fd)
-				
-				if (openedFiles[fd] === undefined)
-				{
-					console.error("File " + fd + " was closed but not opened!")
-					break;
-				}
-				
-				openedFiles[fd].close(function(err)
-				{
-					if (err) throw err
-					
-					delete openedFiles[fd]
-					self.safeWrite(client, "CLOSED " + fd + "\n")
-				})
-				
-				break
 			}
 		}
-		
-		// process next action
-		self.processAction(client, reader, openedFiles)
-	})
+	}
 }
-
-Service.prototype.sendAsset = function(client, id, asset)
-{
-	console.log("sending " + id)
-	
-	// find last revision number
-	var lastRevisionId = Math.max.apply(Math, Object.keys(asset.revisions))
-	var lastRevision = asset.revisions[lastRevisionId]
-	
-	var rights = ""
-	
-	// check reading right (e.g if a complete (maybe old) revision is available)
-	for (var i in asset.revisions)
-	{
-		if (asset.revisions[i].state == "cached")
-		{
-			rights += "r"
-			break
-		}
-	}
-	
-	// check writing right (last revision downloaded + lock)
-	if (asset.lock && (lastRevision.state == "cached"))
-	{
-		if (asset.lock.user == "MrPlop")
-			rights += "w"
-	}
-	
-	this.safeWrite(client, "ASSET " + id + "\n")
-	if (asset.lock)
-	{
-		if (asset.lock.user == "MrPlop")
-			this.safeWrite(client, "lockUser <you>\n")
-		else
-			this.safeWrite(client, "lockUser " + asset.lock.user + "\n")
-		this.safeWrite(client, "lockApp " + asset.lock.application + "\n")
-	}
-	if (rights != "")
-		this.safeWrite(client, "rights " + rights + "\n")
-	this.safeWrite(client, "revision " + lastRevisionId + "\n")
-	this.safeWrite(client, "author " + lastRevision.author + "\n")
-	this.safeWrite(client, "date " + lastRevision.date + "\n")
-	if (lastRevision.path) this.safeWrite(client, "path " + lastRevision.path + "\n")
-	if (lastRevision.type) this.safeWrite(client, "type " + lastRevision.type + "\n")
-	if (lastRevision.length) this.safeWrite(client, "length " + lastRevision.length + "\n")
-	this.safeWrite(client, "END\n")
-}
-
-Service.prototype.sendChunk = function(client, fd, offset, chunk)
-{
-	this.safeWrite(client, "CHUNK " + fd + " " + offset + " " + chunk.length + "\n")
-	this.safeWrite(client, chunk)
-}
-
-Service.prototype.safeWrite = function(client, data)
-{
-	try
-	{
-		client.write(data)
-	}
-	catch (err)
-	{
-		// can happen if the client disconnects *while* writing a bunch of data
-		console.log("ERRORRRRR!!!\n")
-	}
-}*/
